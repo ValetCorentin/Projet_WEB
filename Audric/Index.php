ada135d3f73e8032d9a1d3d70c1aec57d8b777a7
Didicap <audric.capet@viacesi.fr>
2022-03-23 12:42:24 +0100

    Update index.php
    Création de fonctions pour créer une requête SQL

--- Audric/Index.php
+++ Audric/Index.php
@@ -2,11 +2,43 @@
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
 <body>
-je teste
+<h1>Tests du back</h1>
+
+<?php
+  $servername = 'localhost';
+  $dbname = 'projet_chopes_ton_stage';
+  $username = 'root';
+  $password = '';
+// Etablissment de la connexion avec la BDD
+
+  $connexion = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password,[
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ /*PDO::FETCH_OBJ utile pour nettoyer le retour des queries*/
+  ]);
+
+// préparation d'une requête 
+function createQuery{($queryString, $connextion)
+  $query = $connexion->query("SELECT * FROM contact");/*création de la requete*/
+    if($query === false){
+      var_dump($connexion->errorInfo());
+      die('Erreur SQL');
+    }
+}
+
+
+$posts = $query->fetchAll();
+
+?>
+<ul>
+    <?php foreach($posts as $post): ?>
+    <li><a href="./edit.php?id=<?= $post->NoPil?>"><?= htmlentities($post->NoPil).' '.htmlentities($post->NomPil).' | '.htmlentities($post->NatPil) ?></a></li>
+    <?php endforeach ?>
+</ul>
+
 </body>
 </html>
