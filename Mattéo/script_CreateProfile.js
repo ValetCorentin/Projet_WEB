5fcaf769ae4a3d64e211ae56300f8ed2e51a3604
Matt <matteo.fauvel@viacesi.fr>
2022-03-29 13:51:51 +0200

    V6+

--- Mattéo/script_CreateProfile.js
+++ Mattéo/script_CreateProfile.js
@@ -1,34 +1,33 @@
-function enable_checkboxes(){
-  for (var i =0; i < 26; i++){
-    document.getElementsByClassName('delegate_rights_checkboxes')[i].disabled = false;
+function enable_checkboxes() {
+  for (var i = 0; i < 26; i++) {
+    document.getElementsByClassName("delegate_rights_checkboxes")[
+      i
+    ].disabled = false;
   }
 }
 
-function disable_checkboxes(){
-  for (var i =0; i < 26; i++){
-    document.getElementsByClassName('delegate_rights_checkboxes')[i].disabled = true;
+function disable_checkboxes() {
+  for (var i = 0; i < 26; i++) {
+    document.getElementsByClassName("delegate_rights_checkboxes")[
+      i
+    ].disabled = true;
   }
 }
 
-function show_rights(){
+function show_rights() {
   enable_checkboxes();
-  document.getElementById('Delegate_rights').style.display = 'inline';
+  document.getElementById("Delegate_rights").style.display = "inline";
 }
 
-function hide_rights(){
+function hide_rights() {
   disable_checkboxes();
-  document.getElementById('Delegate_rights').style.display = 'none';
+  document.getElementById("Delegate_rights").style.display = "none";
 }
 
-
-
-document.getElementById('Profil_type_Input').onclick = function () {
-if ((document.getElementById('Profil_type_Input').value) == 'Delegate'){
-
-  show_rights();
-  
-}
-else{
-  hide_rights();
+document.getElementById("Profil_type_Input").onclick = function () {
+  if (document.getElementById("Profil_type_Input").value == "Delegate") {
+    show_rights();
+  } else {
+    hide_rights();
+  }
 };
-}
