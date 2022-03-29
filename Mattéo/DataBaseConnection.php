a29fca6713eee35e3e224381af9070982314f2bd
Matt <matteo.fauvel@viacesi.fr>
2022-03-29 19:22:52 +0200

    V7

--- Mattéo/DataBaseConnection.php
+++ Mattéo/DataBaseConnection.php
@@ -5,11 +5,13 @@ $password = '';
 $dbname = 'Projet_WEB';
 
 //Connection to the DB
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-
-$servername = "mysql.hostinger.fr"; 
-$database = "u000000001_nom"; 
-$username = "u000000001_user"; 
-$password = "MotDePasse";
-$sql = "mysql:host=$servername;dbname=$database;";
-$dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+try{
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // Debug connection
+  echo "Connexion établie";
+}
+catch (PDOException $error){
+  // Debug error connection
+  echo "Echec de la connexion: ".$error->getMessage();
+}
+?>
